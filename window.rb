8307e137c345fc9cbf16bfeb7103004609d39da2
App Academy Student <student@appacademy.io>
2016-07-21 18:07:09 -0700

    window first problem

--- window.rb
+++ window.rb
@@ -1,7 +1,11 @@
 def max_window_range(array, window)
   current_max_range = nil
-
+  differences = []
   array.each_with_index do |el, i|
-    
+    next if i + window > array.length
+    sorted_sub = array[i...(i + window)].sort
+    differences << sorted_sub.last - sorted_sub.first
+  end
 
+  differences.sort.last
 end
